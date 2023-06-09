e898cb1e49d3060d935f81ccb8fe7f07f7f0ed19
Nabiha_Nahin <130692302+nabiha02@users.noreply.github.com>
2023-06-10 02:49:36 +0600

    Add files via upload

---
+++ Matrix_from_user_input.c
@@ -0,0 +1,64 @@
+//Making multiple matrix from user input
+
+#include <stdio.h>
+
+int main(void){
+
+  int A[100][100],B[100][100],i,j,no_row,no_col;
+
+  printf("Input your no_row: ");
+  scanf("%d", &no_row);
+  printf("Input your no_col: ");
+  scanf("%d", &no_col);
+
+  //scanning A martrix
+  printf("Enter elements for A matrix\n");
+  for(i=0;i<no_row;i++){
+
+    for(j=0;j<no_col;j++){
+
+      printf("A[%d][%d] = ", i,j);
+      scanf("%d", &A[i][j] );
+    }   
+  }
+
+  //scanning A martrix
+  printf("A = ");
+  for(i=0;i<no_row;i++){
+
+    printf("\t");
+    for(j=0;j<no_col;j++){
+
+        printf("%d ", A[i][j]);
+    }
+
+    printf("\n");
+
+  }
+
+  //scanning B martrix
+  printf("Enter elements for B matrix\n");
+  for(i=0;i<no_row;i++){
+
+    for(j=0;j<no_col;j++){
+
+      printf("B[%d][%d] = ", i,j);
+      scanf("%d", &B[i][j] );
+    }   
+  }
+
+  //scanning B martrix
+  printf("B = ");
+  for(i=0;i<no_row;i++){
+
+    printf("\t");
+    for(j=0;j<no_col;j++){
+
+        printf("%d ", B[i][j]);
+    }
+
+    printf("\n");
+
+  }
+
+}
